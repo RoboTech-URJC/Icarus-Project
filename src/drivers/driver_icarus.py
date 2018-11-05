e87440a49a8ee5e50add2026bd6ac835b712e6c8
Fernando González <39632336+fgonzalezr1998@users.noreply.github.com>
2018-11-05 12:50:51 +0100

    Add files via upload

---
+++ src/drivers/driver_icarus.py
@@ -0,0 +1,117 @@
+import time
+from pymavlink import mavutil
+
+class Drone:
+    def __init__(self, port):
+        '''
+        Port is where drone is listening. As UDP or TCP as Serial
+        '''
+        #Parameters:
+        self.ardupilot_sys_id = 1
+        self.ardupilot_component_id = 1
+
+        #To create la conection:
+        self.ardupilot = mavutil.mavlink_connection(port)
+        self.wait_for_connection()
+
+        #Begin the comunication:
+        self.ardupilot.mav.heartbeat_send(
+        6, # type
+        8, # autopilot
+        192, # base_mode
+        0, # custom_mode
+        4, # system_status
+        1  # mavlink_version
+        )
+
+    #HERE STARTS THE MAIN METHODS
+
+    def arm_disarm(self, state, failsafe):
+        '''
+        arm or disarm the drone depending of the parameter state:
+        if state = 1, then arm drone;
+        if state = 0, then disarm drone;
+        if state != 0 and state != 1, return a message of error
+        failsafe == 1 serves to disarm the drone on flight
+        '''
+        def is_correct(n):
+            return n == 0 or n == 1
+
+        if(is_correct(state) and is_correct(failsafe)):
+
+            if(state == 1):
+                self.ardupilot.set_mode_apm('GUIDED') # set mode flight
+            elif(state == 0):
+                self.ardupilot.set_mode_apm('LAND') # set mode flight
+
+            self.ardupilot.mav.command_long_send(self.ardupilot_sys_id,
+                                            self.ardupilot_component_id,
+                                            mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+                                            0, #confirmation
+                                            state, failsafe, 0, 0, 0, 0, 0)
+
+            if(state == 1):
+                #wait to arm
+                self.ardupilot.motors_armed_wait()
+            elif(state == 0):
+                #wait to disarm
+                self.ardupilot.motors_disarmed_wait()
+
+        else:
+            print("[USAGE ERROR] state value is 1 or 0 for arm/disarm the drone")
+            print("failsafe value is 1/0 for disarm or not disarm the drone on flight")
+
+    def takeoff(self, altitude):
+        '''
+        Take off the drone to a specific altitude
+        '''
+
+        def wait_to_takeoff():
+
+            error_range = 0.08
+
+            alt = self.get_alt()
+            while(alt < altitude - error_range):
+                alt = self.get_alt()
+
+        self.ardupilot.set_mode_apm('GUIDED') # set mode flight
+        self.ardupilot.mav.command_long_send(self.ardupilot_sys_id,
+                                        self.ardupilot_component_id,
+                                        mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
+                                        0, #confirmation
+                                        0, 0, 0, 0, 0, 0, altitude)
+        wait_to_takeoff() #Block the method while drone is taking off
+
+    def land(self):
+        '''
+        Land the drone
+        '''
+
+        def wait_to_land():
+
+            error_range = 0.08
+
+            alt = self.get_alt()
+            while(alt > error_range):
+                alt = self.get_alt()
+
+        self.ardupilot.set_mode_apm('LAND')
+        wait_to_land() #Block the method while drone is landing
+
+    #HERE STARTS THE SECONDARY METHODS
+
+    def wait_for_connection(self):
+        msg= None
+        # wait for autopilot connection
+        while msg is None:
+                msg = self.ardupilot.recv_msg()
+
+        print msg
+
+    def get_alt(self):
+        '''
+        Returns the altitude in meters
+        '''
+
+        param = self.ardupilot.recv_match(type='PARAM_VALUE') #receive all parameters
+        return self.ardupilot.messages['GLOBAL_POSITION_INT'].relative_alt * 0.001
