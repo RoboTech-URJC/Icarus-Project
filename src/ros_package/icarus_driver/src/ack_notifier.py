c88974968dc54c92b5cc08ae1de89cee235a37f0
fgonzalezr1998 <fergonzaramos@yahoo.es>
2019-10-20 03:32:32 +0200

    [ack_notifier] launch params added

--- src/ros_package/icarus_driver/src/ack_notifier.py
+++ src/ros_package/icarus_driver/src/ack_notifier.py
@@ -5,11 +5,24 @@ from std_msgs.msg import String
 
 import serial
 
-ser = serial.Serial("/dev/ttyACM0", 9600)
+class Notifier():
+    def __init__(self):
+        ser_ = None
 
-def callback(data):
-    #b = bytes(data.data)
-    ser.write(data.data)
+        self.init_params()
+
+        rospy.Subscriber('/icarus_driver/ack_notify', String, self.callback)
+
+    def init_params(self):
+
+        port = rospy.get_param('arduino_port', "/dev/ttyACM0")
+        baudrate = rospy.get_param('baudrate', "9600")
+
+        self.ser_ = serial.Serial(port, baudrate)
+
+    def callback(self, data):
+        #b = bytes(data.data)
+        self.ser_.write(data.data)
 
 
 rospy.init_node('ack_notifier_node')
@@ -19,7 +32,8 @@ rate = rospy.Rate(10) #10 Hz
 if __name__ == "__main__":
 
     try:
-        rospy.Subscriber('/icarus_driver/ack_notify', String, callback)
+
+        notifier = Notifier()
         rospy.spin()
 
     except KeyboardInterrupt:
